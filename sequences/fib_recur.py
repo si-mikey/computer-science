ca3e6feb5aa80807a0e73f654f6e62d5681c8143
Luis <mikeylopez@gmail.com>
2018-10-13 04:53:12 -0400

    add for loop and print recursive fib function

--- sequences/fib_recur.py
+++ sequences/fib_recur.py
@@ -1,9 +1,8 @@
 def fib_recur(n):
-    if (n == 0):
-        return 0
-    if (n == 1):
-        return 1
+    if n < 2:
+        return n
     return fib_recur(n - 1) + fib_recur(n - 2)
 
-f = fib_recur(10)
-print(f)
+for n in range(0, 15):
+    print(fib_recur(n))
+
